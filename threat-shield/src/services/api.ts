995fcef536dd2f94ec2749160413564a25e0737f
hpolen <48518015+hpolen@users.noreply.github.com>
2025-11-19 10:31:58 -0500

    add prod backend

--- threat-shield/src/services/api.ts
+++ threat-shield/src/services/api.ts
@@ -7,7 +7,8 @@ import {
   ReportMetadata
 } from '../types/reportTypes';
 
-export const API_BASE_URL = 'http://localhost:5001/api';
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001/api';
 
 export const apiService = {
   getStorageHistory: async (): Promise<{ timestamp: string; data: ReportMetadata[] }> => {
